6954f39e995c702353c5a4f0c10c7affee4c2d20
Milos <milos.marjanovic159@gmail.com>
2025-09-29 14:07:08 +0100

    chat application

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 import { WebSocketServer } from "ws";
+import { v4 } from "uuid";
 
 const wss = new WebSocketServer({ port: 8080 });
 
+const messages = [];
+const players = [];
+
 wss.on("connection", function connection(ws) {
-  ws.on("error", console.error);
-  const messages = [];
+  ws.id = v4();
+  players.push(ws);
 
-  ws.on("message", function message(data) {
-    // console.log("received: %s", data);
 
-    const textString = data.toString("utf8");
-    messages.push(textString);
-    console.log("received: %s", JSON.stringify(messages));
+  if (players.length < 2) {
+    ws.send("Waiting for more players...");
+  } else {
+    ws.send(JSON.stringify(messages));
+  }
+
+  ws.on("message", (message) => {
+    messages.push({ id: ws.id, message: message.toString("utf8") });
+
+    for (const player of players) {
+      if (player.readyState === player.OPEN) {
+        player.send(JSON.stringify(messages));
+      }
+    }
   });
 
-  ws.send(messages);
+  ws.on("error", console.error);
+
+  ws.on("close", () => {
+    console.log("connection closed", ws.id);
+
+    const index = players.findIndex((p) => p.id === ws.id);
+    if (index !== -1) {
+      players.splice(index, 1);
+    }
+  });
 });
